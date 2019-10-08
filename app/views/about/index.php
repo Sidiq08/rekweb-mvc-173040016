c8d052ec26f283d0dc0336a380e6570e56199f6d
Muhammad Aulia Asshiddiqie <m.aulia08@mail.unpas.ac.id>
2019-10-09 05:50:07 +0700

    6 Assets Bootstrap

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>My About</h1>
+<div class="container">
+<h1 class="mt-4">My About</h1>
+<img src="<?= BASEURL; ?>/image/173040016.png" alt="Muhammad Aulia Asshiddiqie" width="200" height="200" class="rounded-circle shadow">
 <p>Halo, Nama saya <?= $data['nama']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
